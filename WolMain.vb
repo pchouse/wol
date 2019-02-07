7273d7a860f3d5890e2c8425951ec236880a56cd
João Rebelo <joao.rebelo@pchouse.pt>
2019-02-07 10:46:36 +0000

    Inicialização de repositório

---
+++ WolMain.vb
@@ -0,0 +1,29 @@
+﻿Module WolMain
+
+    Sub Main(ByVal args As String())
+
+        Try
+            Dim macAddress(6) As Byte
+            If args.Length = 1 Then
+                Dim mac() As String = args(0).Split(":")
+                If mac.Length <> 6 Then
+                    Throw New Exception("MAC address should be in format 00:00:00:00:00:00")
+                End If
+
+                For n As Integer = 0 To 5 Step 1
+                    macAddress(n) = CByte("&H" + mac(n))
+                Next
+                Dim w As New wol(macAddress)
+            Else
+                Throw New Exception("You must specifie the MAC address")
+            End If
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+
+
+    End Sub
+
+End Module
---
+++ wol.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+
+Public Class wol
+
+    Public Sub New(ByVal macAddress As Byte())
+        Me.sendWOL(macAddress)
+    End Sub
+
+
+    Public Sub sendWOL(ByVal macAddress As Byte())
+
+        Dim client As New UdpClient
+
+        'Dim buf(101) As Char
+
+        Dim sendBytes(101) As Byte '= System.Text.Encoding.ASCII.GetBytes(buf)
+
+        For x As Integer = 0 To 5
+            sendBytes(x) = CByte(&HFF)
+        Next
+
+        Dim i As Integer = 6
+
+        For x As Integer = 1 To 16
+
+            sendBytes(i) = CByte(macAddress(0))
+
+            sendBytes(i + 1) = CByte(macAddress(1))
+
+            sendBytes(i + 2) = CByte(macAddress(2))
+
+            sendBytes(i + 3) = CByte(macAddress(3))
+
+            sendBytes(i + 4) = CByte(macAddress(4))
+
+            sendBytes(i + 5) = CByte(macAddress(5))
+
+            i += 6
+
+        Next
+        client.Connect(IPAddress.Broadcast, 7)
+        client.Send(sendBytes, sendBytes.Length)
+    End Sub
+
+
+
+End Class
